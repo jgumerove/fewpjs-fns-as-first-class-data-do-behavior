491b377388cde6419940c9fa0f96b645f9783af7
jgumerove <josh.gumerove@gmail.com>
2021-08-05 16:23:32 -0400

    commit tree 2733fa905661aa875c3d47b7b022b2fb2329ce16

--- index.js
+++ index.js
@@ -11,3 +11,21 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(string) {
+  let newString = string.split(':').join('')
+  let toInteger = parseInt(newString)
+  if (toInteger < 1200) {
+    return "Good Morning"
+  }
+  if (toInteger >= 1200 && toInteger <= 1700) {
+    return "Good Afternoon"
+  }
+  if (toInteger > 1700) {
+    return "Good Evening"
+  }
+}
+
+function displayMessage(greet) {
+  document.getElementById('greeting').innerHTML = greet
+}
